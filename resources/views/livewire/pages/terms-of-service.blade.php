f62e2d57f52c2d836554cfd695788fa9081a108b
Umar-Farooq-Shafi <umarfarooqshafi2101@gmail.com>
2024-10-16 21:56:24 +0500

    terms

--- resources/views/livewire/pages/terms-of-service.blade.php
+++ resources/views/livewire/pages/terms-of-service.blade.php
@@ -1,6 +1,5 @@
-<div class="mt-40">
-    <div
-        class="flex flex-col gap-y-1 md:flex-row justify-between bg-gray-300 px-4 py-2 md:rounded md:mx-16 lg:mx-32 my-4">
+<div class="mt-[25%] lg:mt-[12%] md:mt-[17%] container mx-auto">
+    <div class="flex flex-col gap-y-1 md:flex-row justify-between bg-gray-300 px-4 py-2 md:rounded my-4">
         <div class="font-bold text-xl">Terms of Service</div>
 
         <x-breadcrumbs/>
