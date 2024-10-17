96c8a13fb31deb33cc914d3009126fda96f2545d
Umar-Farooq-Shafi <umarfarooqshafi2101@gmail.com>
2024-10-17 22:49:11 +0500

    Update LinkTrait.php

--- app/Traits/LinkTrait.php
+++ app/Traits/LinkTrait.php
@@ -5,13 +5,18 @@ namespace App\Traits;
 use App\Filament\Resources\EventResource\Pages\CreateEvent;
 use App\Filament\Resources\OrderResource\Pages\ListOrders;
 use App\Models\BlogPost;
-use App\Models\Category;
+use App\Models\BlogPostTranslation;
+use App\Models\CategoryTranslation;
 use App\Models\Event;
+use App\Models\EventTranslation;
 use App\Models\HelpCenterArticle;
+use App\Models\HelpCenterArticleTranslation;
 use App\Models\HelpCenterCategory;
-use App\Models\Page;
+use App\Models\HelpCenterCategoryTranslation;
+use App\Models\PageTranslation;
 use App\Models\User;
 use App\Models\Venue;
+use App\Models\VenueTranslation;
 
 trait LinkTrait
 {
@@ -19,97 +24,110 @@ trait LinkTrait
     {
         $linksArray = [];
 
-        $staticPages = Page::all();
+        // Add static pages
         $staticPagesArray = [
             route('home', [], false) => __('Homepage'),
+            route('contact-us', [], false) => __('Contact Us')
         ];
-
-        foreach ($staticPages as $staticPage) {
+        foreach (
+            PageTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'title'])->get()
+            as $staticPage
+        ) {
             $staticPagesArray[route($staticPage->slug, [], false)] = $staticPage->title;
         }
-
-        $staticPagesArray[route('contact-us', [], false)] = __('Contact Us');
         $linksArray[__('Static Pages')] = $staticPagesArray;
 
-        // Add authentication pages urls
-        $authentificationPagesArray = [
+        // Authentication pages
+        $linksArray[__('Authentication Pages')] = [
             route('filament.admin.auth.login', [], false) => __('Login'),
             route('filament.admin.auth.password-reset.request', [], false) => __('Password Resetting'),
             route('filament.admin.auth.register', [], false) => __('Attendee Registration'),
             route('filament.admin.auth.register', [], false) => __('Organizer Registration'),
         ];
-        $linksArray[__('Authentication Pages')] = $authentificationPagesArray;
 
-        // Add dashboard pages urls
-        $dashboardPagesArray = [
+        // Dashboard pages
+        $linksArray[__('Dashboard Pages')] = [
             ListOrders::getUrl(isAbsolute: false) => __('Attendee tickets'),
             CreateEvent::getUrl(isAbsolute: false) => __('Create event'),
         ];
-        $linksArray[__('Dashboard Pages')] = $dashboardPagesArray;
 
-        // Add category pages urls
+        // Event categories
         $categoryPagesArray = [
             'categories_dropdown' => __('Categories Dropdown'),
-            'footer_categories_section' => __('Footer Categories Section'),
+            'footer_categories_section' => __('Footer Categories Section')
         ];
-
-        $categories = Category::all(); // Replace with your actual method to retrieve categories
-        foreach ($categories as $category) {
+        foreach (
+            CategoryTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'name'])->get()
+            as $category
+        ) {
             $categoryPagesArray[route('events', ['category' => $category->slug], false)] = __('Category') . ' - ' . $category->name;
         }
         $linksArray[__('Event Categories')] = $categoryPagesArray;
 
-        // Add blog post pages urls
+        // Blog posts
         $blogPagesArray = [
             route('blog', [], false) => __('Blog page'),
         ];
-
-        $blogPosts = BlogPost::all(); // Replace with your actual method to retrieve blog posts
-        foreach ($blogPosts as $blogPost) {
+        foreach (
+            BlogPostTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'name'])->get()
+            as $blogPost
+        ) {
             $blogPagesArray[route('blog-article', ['slug' => $blogPost->slug], false)] = __('Blog post') . ' - ' . $blogPost->name;
         }
         $linksArray[__('Blog Pages')] = $blogPagesArray;
 
-        // Add event pages urls
+        // Events
         $eventPagesArray = [
             route('events', [], false) => __('Events page'),
         ];
-
-        $events = Event::all(); // Replace with your actual method to retrieve events
-        foreach ($events as $event) {
+        foreach (
+            EventTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'name'])->get()
+                as $event
+        ) {
             $eventPagesArray[route('event', ['slug' => $event->slug], false)] = __('Event') . ' - ' . $event->name;
         }
         $linksArray[__('Events Pages')] = $eventPagesArray;
 
-        // Add help center pages urls
+        // Help center
         $helpCenterPagesArray = [
             route('help-center', [], false) => __('Help Center page'),
         ];
-
-        $helpCenterCategories = HelpCenterCategory::all(); // Replace with your actual method to retrieve help center categories
-        $helpCenterArticles = HelpCenterArticle::all(); // Replace with your actual method to retrieve help center articles
-        foreach ($helpCenterCategories as $helpCenterCategory) {
-            $helpCenterPagesArray[route('help-center-category', ['slug' => $helpCenterCategory->slug], false)] = __('Help Center Category') . ' - ' . $helpCenterCategory->name;
+        foreach (
+            HelpCenterCategoryTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'name'])->get()
+                as $category
+        ) {
+            $helpCenterPagesArray[route('help-center-category', ['slug' => $category->slug], false)] = __('Help Center Category') . ' - ' . $category->name;
         }
-        foreach ($helpCenterArticles as $helpCenterArticle) {
-            $helpCenterPagesArray[route('help-center-article', ['slug' => $helpCenterArticle->slug], false)] = __('Help Center Article') . ' - ' . $helpCenterArticle->title;
+        foreach (
+            HelpCenterArticleTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'title'])->get()
+                as $article
+        ) {
+            $helpCenterPagesArray[route('help-center-article', ['slug' => $article->slug], false)] = __('Help Center Article') . ' - ' . $article->title;
         }
         $linksArray[__('Help Center Pages')] = $helpCenterPagesArray;
 
-        // Add organizers pages urls
+        // Organizers
         $organizersPagesArray = [];
-        $organizers = User::query()->whereHas('organizer')->get();
-        foreach ($organizers as $organizer) {
+        foreach (User::whereHas('organizer')->with('organizer')->get() as $organizer) {
             $organizersPagesArray[route('organizer-profile', ['slug' => $organizer->organizer->slug], false)] = __('Organizer Profile') . ' - ' . $organizer->organizer->name;
         }
         $linksArray[__('Organizers Pages')] = $organizersPagesArray;
 
-        // Add venues pages urls
+        // Venues
         $venuesPagesArray = [
             route('venues', [], false) => __('Venues page'),
         ];
-        $venues = Venue::all(); // Replace with your actual method to retrieve venues
-        foreach ($venues as $venue) {
+        foreach (
+            VenueTranslation::query()->where('locale', app()->getLocale())
+                ->select(['slug', 'name'])->get()
+                as $venue
+        ) {
             $venuesPagesArray[route('venue', ['slug' => $venue->slug], false)] = __('Venue') . ' - ' . $venue->name;
         }
         $linksArray[__('Venues Pages')] = $venuesPagesArray;
